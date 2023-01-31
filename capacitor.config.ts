7d8b6dfb02b7f0fa9aa09ff37e60e1c1a9e1ab1d
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-01-31 11:05:28 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'vlille-finder',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
