83f9d8d48ad7399033f99c4da10b5b2230c77857
FlorianFlouquet <florian.flouquet2@hotmail.com>
2023-01-31 16:42:10 +0100

    plus jamais ionic par pitié

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,29 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonPage, } from '@ionic/react';
 import './Home.css';
+import 'leaflet/dist/leaflet.css'
+import { MapContainer, TileLayer } from "react-leaflet" ;
+import { useLayoutEffect, useState } from 'react';
 
 const Home: React.FC = () => {
+
+  const [renderMap, setRenderMap] = useState(false);
+
+
+  useLayoutEffect(() => {
+    setTimeout(() => setRenderMap(true), 1000);
+  }, [])
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        {renderMap && 
+          <MapContainer style={{width: "100%", height: "100%"}} center={[50.62972, 3.133873]} zoom={12}>
+            <TileLayer 
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+            />
+          </MapContainer> 
+        }
       </IonContent>
     </IonPage>
   );
